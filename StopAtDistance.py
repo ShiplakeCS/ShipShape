1f0d1218ab822480e84321b6b055d97c3d9d8f5e
ShiplakeCS <computing@shiplake.org.uk>
2020-05-13 13:19:58 +0100

    Sort of works, but need some tweaks

--- StopAtDistance.py
+++ StopAtDistance.py
@@ -1,22 +1,28 @@
 # Written by Sam Wright
 
-import time 
+import time
 from gpiozero import DistanceSensor
 from gpiozero import CamJamKitRobot
 
 pinTrigger = 17
 pinEcho = 18
-distanceToStop = 0.05
+distanceToStop = 0.5
 robot = CamJamKitRobot()
 
 sensor = DistanceSensor(echo=pinEcho,trigger=pinTrigger)
 
+x = 0
+    
 try:
     while True:
         print("Distance:",sensor.distance)
-        robot.forward()
-        if sensor.distance <= distanceToStop:
+        if sensor.distance >= distanceToStop:
+            robot.forward()
+            print("{}: forward".format(x))
+            x+=1
+        else:
             robot.stop()
-            break
+        time.sleep(0.1)
 except KeyboardInterrupt:
     print("finished")
+
